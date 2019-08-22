eaf429fcaeb68ed267e724cd4e68b93a908427bb
Jeff Vogt <jvogt@chef.io>
2019-08-21 23:42:32 -0700

    updates

--- test/scripts/provisioner.sh
+++ test/scripts/provisioner.sh
@@ -25,11 +25,24 @@ echo "Determine pkg_prefix for $pkg_artifact"
 pkg_prefix=$(find "/hab/pkgs/$pkg_origin/$pkg_name" -maxdepth 2 -mindepth 2 | sort | tail -n 1)
 echo "Found: $pkg_prefix"
 
+cd ~
+echo "Starting Hab Sup in `pwd`"
+nohup sudo -E /hab/bin/hab sup run &
+sleep 10
+
+echo "Adding mongo conf"
+echo '[mongod]
+
+[mongod.net]
+bind_ip = "0.0.0.0"
+
+[mongod.security]
+cluster_auth_mode = ""' > /tmp/mongo.toml
+/hab/bin/hab config apply mongodb.default $(date +%s) /tmp/mongo.toml
+
 # Execute Chef Infra client inside the package
 echo "Running chef-client in $pkg_origin/$pkg_name"
 cd "$pkg_prefix" || exit 1
-nohup sudo -E /hab/bin/hab sup run &
-sleep 10
 sudo -E /hab/bin/hab svc load "core/mongodb/3.2.9/20170515233318"
 sudo -E /hab/bin/hab svc load "ericheiser/national-parks-datadog" --bind database:mongodb.default
 sudo -E /hab/bin/hab pkg exec "$pkg_origin/$pkg_name" chef-client -z -c "$pkg_prefix/config/bootstrap-config.rb"
